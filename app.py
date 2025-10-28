1ca7fd615fb7a36db889ed05281c3456040c64eb
RapoMG <rapomg@gmail.com>
2025-10-28 01:18:11 +0100

    Multiple screens

--- app.py
+++ app.py
@@ -1,31 +1,45 @@
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.lang import Builder
-from kivy.uix.widget import Widget
+from kivy.uix.screenmanager import Screen, ScreenManager
 
-Window.size = (450,800)
-#Window.size = (1080,1920)
 
-class MainLayout(Widget):
-    Builder.load_file("plrs_number.kv")
+# Window size
+Window.size = (450,800) # (1080,1920)
+
+### Multiple Screens
+#First screen (page)
+class PlayersWindow(Screen):
 
     def incr(self):
-        #increas number up to 4
+        #increases number up to 4
         number = int(self.ids.n_players.text)
         if number < 4:
             number += 1
         self.ids.n_players.text = str(number)
 
     def decr(self):
-        #decreas value down to 2
+        #decreases value down to 2
         number = int(self.ids.n_players.text)
         if number > 2:
             number -= 1
         self.ids.n_players.text = str(number)
 
+#second screen (page)
+class FactionsWindow(Screen):
+    pass
+
+#thrid screen (page)
+class DrawResultsWindow(Screen):
+    pass
+
+class WindowManager(ScreenManager):
+    pass
+
+
 class SelectorApp(App):
     def build(self):
-        return MainLayout()
+        return Builder.load_file("plrs_number.kv")
 
 if __name__ == "__main__":
     SelectorApp().run()
